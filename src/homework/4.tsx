cc4362f561d7b5ad4b1f81e73785d2b48a3671fa
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2024-01-06 12:51:49 +0200

    ts

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,28 +1,39 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
+import React, {
+  createContext,
+  useMemo,
+  useState,
+  useContext,
+  ReactNode,
+} from "react";
 import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
-// Додати тип Menu Selected
+const baseMenu: Menu = { id: "first", title: "default" };
 
-const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+type SelectedMenu = { id: MenuIds };
+
+type MenuAction = {
+  onSelectedMenu: React.Dispatch<React.SetStateAction<SelectedMenu>>;
+};
+
+type PropsProvider = {
+  children: ReactNode;
+};
+
+const MenuSelectedContext = createContext<{
+  selectedMenu: SelectedMenu;
+}>({
+  selectedMenu: baseMenu,
 });
-
-// Додайте тип MenuAction
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
-type PropsProvider = {
-  children; // Додати тип для children
-};
-
 function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>(baseMenu);
 
   const menuContextAction = useMemo(
     () => ({
@@ -48,7 +59,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[];
 };
 
 function MenuComponent({ menus }: PropsMenu) {
